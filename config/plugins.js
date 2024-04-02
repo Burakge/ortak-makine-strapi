07c14dc4be27bdeee068a8c9099b46bd2cc2821f
burakge <brkegen@gmail.com>
2024-04-02 08:31:04 +0300

    strapi-providers-upload-fix

--- config/plugins.js
+++ config/plugins.js
@@ -7,7 +7,9 @@ module.exports = ({env}) => ({
         key: env('DO_SPACE_ACCESS_KEY'),
         secret: env('DO_SPACE_SECRET_KEY'),
         endpoint: env('DO_SPACE_ENDPOINT'),
-        space: env('DO_SPACE_BUCKET')
+        space: env('DO_SPACE_BUCKET'),
+        directory: env('DO_SPACE_DIRECTORY'),
+        cdn: env('DO_SPACE_CDN'),
       }
     },
   }, 
